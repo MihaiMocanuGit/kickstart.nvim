4aa7fb73b106202850da739e20768cc734de84de
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-04-27 15:24:46 +0300

    Added keymap for invoking a new terminal window.

--- init.lua
+++ init.lua
@@ -203,6 +203,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- CUSTOM KEYMAPS
+-- Create new terminal window
+vim.keymap.set('n', '<leader>nt', '<cmd> new <bar> term <CR> i', { desc = 'Create [N]ew [T]erminal window' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
