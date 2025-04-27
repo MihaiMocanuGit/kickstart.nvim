124225304379b3d65d7350e68c88c4e0c0b69353
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-04-27 14:17:46 +0300

    Added and configured C/C++ debugger.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'williamboman/mason.nvim',
+    opts = {
+      ensure_installed = {
+        'clangd',
+        'clang-format',
+        'codelldb',
+      },
+    },
+  },
+}
