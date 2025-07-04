3862e45054f1ae68257395b345f05cdd5b68d98a
MihaiMocanuGit <mihai.mocanu.0220@gmail.com>
2025-07-04 19:29:05 +0300

    Defined tab size

--- init.lua
+++ init.lua
@@ -98,6 +98,13 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- Set tab to 4 characters
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+-- Always convert tab to spaces
+vim.o.expandtab = true
+
 -- Make line numbers default
 vim.o.number = true
 vim.o.relativenumber = true
@@ -556,6 +563,20 @@ require('lazy').setup({
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+
+          -- Find references for the word under your cursor.
+          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+          -- Jump to the definition of the word under your cursor.
+          --  This is where a variable was first declared, or where a function is defined, etc.
+          --  To jump back, press <C-t>.
+          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+          map('gsh', '<cmd> LspClangdSwitchSourceHeader <cr>', '[G]oto [S]ource<->[H]eader')
 
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
